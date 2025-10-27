3d7e7f4d8e82ac2a047fe82f62d11dfddd59fa8c
Gunics Roland <grolcsi09@gmail.com>
2025-10-27 16:03:15 +0100

    feat(main): added console arg handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include "../include/input_handler.hpp"
 #include "../include/preprocessor.hpp"
+#include <iostream>
 
-auto main() -> int {
-    input_handler handler("./bin/input.txt", "./bin/output.txt");
+auto main(int argc, char *argv[]) -> int {
+    std::string input_file;
+    std::string output_file;
+
+    if(argc == 2){
+        input_file = std::string(argv[1]);
+        output_file = "./o.txt";
+    }
+    if(argc == 3){
+        input_file = std::string(argv[1]);
+        output_file = std::string(argv[2]);
+    }
+    else{
+        std::cout << "Wrong usage of mock compiler" << '\n';
+        std::cout << "compiler input_file [output_file]" << '\n';
+    }
+
+    input_handler handler(input_file, output_file);
     preprocessor preprocessor;
 
     handler.open_file();
