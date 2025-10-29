c67127e1d22af1adc4c52b4346f05cf4b1f1ca6e
Gunics Roland <grolcsi09@gmail.com>
2025-10-29 12:53:58 +0100

    feat(enums): token_type enum for more efficient handling

---
+++ include/enums.hpp
@@ -0,0 +1,41 @@
+#ifndef ENUM_HPP
+#define ENUM_HPP
+
+enum class token_type {
+    IF,
+    ELSE,
+    WHILE,
+    FOR,
+    RETURN,
+    FUNCTION,
+    VAR,
+    LET,
+    CONST,
+    TRUE_,
+    FALSE_,
+    NULL_,
+    FLOAT_LITERAL,
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LPAREN,
+    RPAREN,
+    SEMICOLON,
+    EQUALS,
+    NOT_EQUALS,
+    LESS_THAN_OR_EQUAL,
+    GREATER_THAN_OR_EQUAL,
+    LESS_THAN,
+    GREATER_THAN,
+    ASSIGN,
+    LBRACE,
+    RBRACE,
+    COMMA,
+    IDENTIFIER,
+    STRING_LITERAL,
+    CHARACTER_LITERAL
+};
+
+#endif
